6b6cbf7745d160f4577b2b5d4d13435072c5f244
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2021-10-15 01:40:44 +0300

    split class My on Color and Util

--- src/Color.java
+++ src/Color.java
@@ -14,27 +14,6 @@ public class Color {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    //BOLD
-    public static final String ANSI_BOLD_BLACK =    "\033[1;30m";  // BLACK
-    public static final String ANSI_BOLD_RED =      "\033[1;31m";    // RED
-    public static final String ANSI_BOLD_GREEN =    "\033[1;32m";  // GREEN
-    public static final String ANSI_BOLD_YELLOW =   "\033[1;33m"; // YELLOW
-    public static final String ANSI_BOLD_BLUE =     "\033[1;34m";   // BLUE
-    public static final String ANSI_BOLD_PURPLE =   "\033[1;35m"; // PURPLE
-    public static final String ANSI_BOLD_CYAN =     "\033[1;36m";   // CYAN
-    public static final String ANSI_BOLD_WHITE =    "\033[1;37m";  // WHITE
-
-    //
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-    //
-
     private Color(){
 
     }
